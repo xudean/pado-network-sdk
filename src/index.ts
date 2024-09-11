c25c875c42d42242d362e37ebc5409ff7eefd6e3
Dean Xu <xuda.it@outlook.com>
2024-09-11 10:13:31 +0800

    export modules

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,8 @@
 
 // export { padonetwork };
 // export * from './padonetwork';
-import { ChainName, CommonObject, EncryptionSchema, PriceInfo, StorageType, TaskType } from './types';
-export { ChainName, CommonObject, EncryptionSchema, PriceInfo, StorageType, TaskType };
-import {EthereumMetamaskConnector,ArweaveArConnectConnector} from './types';
-export {EthereumMetamaskConnector,ArweaveArConnectConnector};
+export * from './types';
+export * from './types/connectors';
 import PadoNetworkContractClient from './pado-network-contract-client/index';
 import Utils from './common/utils';
 export {Utils};
